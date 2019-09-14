6afd06ad59b5df76438454b16b9442600cd977d8
bhanu <bhanu@spotdraft.com>
2019-09-14 18:41:29 +0530

    add initial code to convert raw data to torch dataset

---
+++ data_builder.py
@@ -0,0 +1,152 @@
+import re
+from pytorch_transformers import BertTokenizer
+
+
+def cal_rouge(evaluated_ngrams, reference_ngrams):
+    reference_count = len(reference_ngrams)
+    evaluated_count = len(evaluated_ngrams)
+
+    overlapping_ngrams = evaluated_ngrams.intersection(reference_ngrams)
+    overlapping_count = len(overlapping_ngrams)
+
+    if evaluated_count == 0:
+        precision = 0.0
+    else:
+        precision = overlapping_count / evaluated_count
+
+    if reference_count == 0:
+        recall = 0.0
+    else:
+        recall = overlapping_count / reference_count
+
+    f1_score = 2.0 * ((precision * recall) / (precision + recall + 1e-8))
+    return {"f": f1_score, "p": precision, "r": recall}
+
+
+def _get_ngrams(n, text):
+    """Calcualtes n-grams.
+    Args:
+      n: which n-grams to calculate
+      text: An array of tokens
+    Returns:
+      A set of n-grams
+    """
+    ngram_set = set()
+    text_length = len(text)
+    max_index_ngram_start = text_length - n
+    for i in range(max_index_ngram_start + 1):
+        ngram_set.add(tuple(text[i:i + n]))
+    return ngram_set
+
+
+def _get_word_ngrams(n, sentences):
+    """Calculates word n-grams for multiple sentences.
+    """
+    assert len(sentences) > 0
+    assert n > 0
+
+    # words = _split_into_words(sentences)
+
+    words = sum(sentences, [])
+    # words = [w for w in words if w not in stopwords]
+    return _get_ngrams(n, words)
+
+
+def greedy_selection(doc_sent_list, abstract_sent_list, summary_size):
+    def _rouge_clean(s):
+        return re.sub(r'[^a-zA-Z0-9 ]', '', s)
+    max_rouge = 0.0
+    abstract = sum(abstract_sent_list, [])
+    abstract = _rouge_clean(' '.join(abstract)).split()
+    # print(abstract)
+    sents = [_rouge_clean(' '.join(s)).split() for s in doc_sent_list]
+    # print(sents)
+    evaluated_1grams = [_get_word_ngrams(1, [sent]) for sent in sents]
+    reference_1grams = _get_word_ngrams(1, [abstract])
+    evaluated_2grams = [_get_word_ngrams(2, [sent]) for sent in sents]
+    reference_2grams = _get_word_ngrams(2, [abstract])
+
+    selected = []
+    for s in range(summary_size):
+        cur_max_rouge = max_rouge
+        cur_id = -1
+        for i in range(len(sents)):
+            if i in selected:
+                continue
+            c = selected + [i]
+            candidates_1 = [evaluated_1grams[idx] for idx in c]
+            candidates_1 = set.union(*map(set, candidates_1))
+            # print(candidates_1)
+            candidates_2 = [evaluated_2grams[idx] for idx in c]
+            # print(candidates_2)
+            candidates_2 = set.union(*map(set, candidates_2))
+            rouge_1 = cal_rouge(candidates_1, reference_1grams)['f']
+            rouge_2 = cal_rouge(candidates_2, reference_2grams)['f']
+            rouge_score = rouge_1 + rouge_2
+
+            # print(f"current rouge score: {rouge_score}")
+            # print(f"max rouge score: {cur_max_rouge}")
+            if rouge_score > cur_max_rouge:
+                cur_max_rouge = rouge_score
+                cur_id = i
+        # print(f"selected {cur_id}")
+        # print("-"*100)
+        if cur_id == -1:
+            return selected
+        selected.append(cur_id)
+        max_rouge = cur_max_rouge
+
+    return sorted(selected)
+
+
+class BertData:
+    def __init__(self):
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+        self.sep_vid = self.tokenizer.vocab['[SEP]']
+        self.cls_vid = self.tokenizer.vocab['[CLS]']
+        self.pad_vid = self.tokenizer.vocab['[PAD]']
+
+    def preprocess(self, src, tgt, oracle_ids):
+
+        if len(src) == 0:
+            return None
+
+        original_src_txt = [' '.join(s) for s in src]
+
+        labels = [0] * len(src)
+        for l in oracle_ids:
+            labels[l] = 1
+
+        idxs = [i for i, s in enumerate(src)]
+
+        src = [src[i][:] for i in idxs]
+        labels = [labels[i] for i in idxs]
+        src = src[:]
+        labels = labels[:]
+
+        if len(labels) == 0:
+            return None
+
+        src_txt = [' '.join(sent) for sent in src]
+        # text = [' '.join(ex['src_txt'][i].split()[:self.args.max_src_ntokens]) for i in idxs]
+        # text = [_clean(t) for t in text]
+        text = ' [SEP] [CLS] '.join(src_txt)
+        src_subtokens = self.tokenizer.tokenize(text)
+        src_subtokens = src_subtokens[:510]
+        src_subtokens = ['[CLS]'] + src_subtokens + ['[SEP]']
+
+        src_subtoken_idxs = self.tokenizer.convert_tokens_to_ids(src_subtokens)
+        _segs = [-1] + [i for i, t in enumerate(src_subtoken_idxs) if t == self.sep_vid]
+        segs = [_segs[i] - _segs[i - 1] for i in range(1, len(_segs))]
+        segments_ids = []
+        for i, s in enumerate(segs):
+            if i % 2 == 0:
+                segments_ids += s * [0]
+            else:
+                segments_ids += s * [1]
+        cls_ids = [i for i, t in enumerate(src_subtoken_idxs) if t == self.cls_vid]
+        labels = labels[:len(cls_ids)]
+
+        tgt_txt = '<q>'.join([' '.join(tt) for tt in tgt])
+        src_txt = [original_src_txt[i] for i in idxs]
+        return src_subtoken_idxs, labels, segments_ids, cls_ids, src_txt, tgt_txt
